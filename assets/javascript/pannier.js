505b2580c5966af45e68d4aea6f3207ca9159657
nfcdjobo <nfcdjobo@gmail.com>
2023-02-24 05:25:16 +0000

    Ajout au panier

--- assets/javascript/pannier.js
+++ assets/javascript/pannier.js
@@ -51,12 +51,12 @@ let clef = 0;
 if (locals.length>=1){
     let toutajout=0;
     for(const denombrement in locals){
-        if (clef <= (locals.length-1)){
+        if (clef <= (locals.length - 1) && !isNaN(parseInt((JSON.parse(locals[denombrement]).Quantite)))){
             toutajout +=parseInt(JSON.parse(locals[denombrement]).Quantite);
             clef++;
+            document.getElementById("panier-compte").textContent = toutajout;
         }
     }
-    document.getElementById("panier-compte").textContent = toutajout;
 }
 
 add_cart.forEach(card_element => {
